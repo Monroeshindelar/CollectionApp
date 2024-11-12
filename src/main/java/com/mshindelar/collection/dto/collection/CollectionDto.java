245f3c475a6aefe84c4211fb8f3d4b185f4686bc
Monroe Shindelar <monroeshindelar@gmail.com>
2024-11-11 21:36:50 -0800

    Adding the ability to filter collections and all cards

--- src/main/java/com/mshindelar/collection/dto/collection/CollectionDto.java
+++ src/main/java/com/mshindelar/collection/dto/collection/CollectionDto.java
@@ -1,17 +1,13 @@
 package com.mshindelar.collection.dto.collection;
 
 import com.mshindelar.collection.model.collection.CollectionSettings;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CollectionDto {
